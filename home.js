b04d2f9f8bafed1661fc8350f94cda661288a1ce
Hamim Hossain <hamim404@proton.me>
2026-06-20 14:10:58 +0600

    Toggling funtion and btn active funtion completed

--- home.js
+++ home.js
@@ -24,36 +24,86 @@ function setInnerText(value) {
   const balanceElement = document.getElementById("main-balance");
   balanceElement.innerText = value;
 }
+// Toggle Active Function
+function btnActive(id) {
+  const btns = document.getElementsByClassName("btns");
+  for (const btn of btns) {
+    btn.classList.remove("border-sky-500", "text-sky-500");
+  }
+  document.getElementById(id).classList.add("border-sky-500", "text-sky-500");
+}
 // All Section
 const addSection = document.getElementById("add-money-section");
 const cashoutSection = document.getElementById("cashout-section");
+const transferMoneySection = document.getElementById("transfer-money-section");
+const getBonusSection = document.getElementById("get-bonus-section");
+const payBillSection = document.getElementById("pay-bill-section");
+const transactionsSection = document.getElementById("transactions-section");
 
 // Toggling Feature
-
 // add-money
 document
   .getElementById("add-money-btn")
   .addEventListener("click", function (e) {
-    console.log("Add Clicked");
+    const forms = document.getElementsByClassName("forms");
+    for (const form of forms) {
+      form.style.display = "none";
+    }
+    btnActive("add-money-btn");
     addSection.style.display = "block";
-    cashoutSection.style.display = "none";
   });
 
-// withdraw-money
+//cashout-section
 document.getElementById("cashout-btn").addEventListener("click", function (e) {
-  console.log("Cashout Clicked");
-  document
-    .getElementById("add-money-btn")
-    .classList.add(
-      "text-sky-500",
-      "border-sky-500",
-      "border-solid",
-      "border",
-      "font-bold",
-    );
+  const forms = document.getElementsByClassName("forms");
+  for (const form of forms) {
+    form.style.display = "none";
+  }
+  btnActive("cashout-btn");
   cashoutSection.style.display = "block";
-  addSection.style.display = "none";
 });
+// transfer-money-section
+document
+  .getElementById("transfer-money-btn")
+  .addEventListener("click", function (e) {
+    const forms = document.getElementsByClassName("forms");
+    for (const form of forms) {
+      form.style.display = "none";
+    }
+    btnActive("transfer-money-btn");
+    transferMoneySection.style.display = "block";
+  });
+// Get-Bonus
+document
+  .getElementById("get-bonus-btn")
+  .addEventListener("click", function (e) {
+    const forms = document.getElementsByClassName("forms");
+    for (const form of forms) {
+      form.style.display = "none";
+    }
+    btnActive("get-bonus-btn");
+    getBonusSection.style.display = "block";
+  });
+// pay-bill
+document.getElementById("pay-bill-btn").addEventListener("click", function (e) {
+  const forms = document.getElementsByClassName("forms");
+  for (const form of forms) {
+    form.style.display = "none";
+  }
+  btnActive("pay-bill-btn");
+  payBillSection.style.display = "block";
+});
+// transactions
+document
+  .getElementById("transactions-btn")
+  .addEventListener("click", function (e) {
+    const forms = document.getElementsByClassName("forms");
+    for (const form of forms) {
+      form.style.display = "none";
+    }
+    btnActive("transactions-btn");
+    transactionsSection.style.display = "block";
+  });
 
 // Add Money Function
 document.getElementById("add-btn").addEventListener("click", function (e) {
